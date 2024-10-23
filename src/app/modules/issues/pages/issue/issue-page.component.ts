c64e0bb4bee3af82e14d57654229f8b4ed2768aa
C.Manuel Hernández <cmanuelhm87@gmail.com>
2024-10-23 16:45:24 +0200

    btener issue por número

--- src/app/modules/issues/pages/issue/issue-page.component.ts
+++ src/app/modules/issues/pages/issue/issue-page.component.ts
@@ -1,5 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { map, tap } from 'rxjs';
+import { IssueService } from '../../services/issue.service';
 
 @Component({
   selector: 'app-issue-page',
@@ -7,4 +11,14 @@ import { Component } from '@angular/core';
   imports: [CommonModule],
   templateUrl: './issue-page.component.html',
 })
-export default class IssuePageComponent {}
+export default class IssuePageComponent {
+  route = inject(ActivatedRoute);
+  issueService = inject(IssueService);
+
+  issueNumber = toSignal<string>(
+    this.route.paramMap.pipe(
+      map((params) => params.get('number') ?? ''),
+      tap((number) => this.issueService.setIssueNumber(number))
+    )
+  );
+}
