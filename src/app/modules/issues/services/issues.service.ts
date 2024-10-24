17f1952ec2fa39478a3459f09de6ca7f5223ef21
C.Manuel Hernández <cmanuelhm87@gmail.com>
2024-10-24 09:19:51 +0200

    Condición completa - Filtrar por estado y etiquetas

--- src/app/modules/issues/services/issues.service.ts
+++ src/app/modules/issues/services/issues.service.ts
@@ -8,6 +8,7 @@ import { State } from '../interfaces';
 })
 export class IssuesService {
   selectedState = signal<State>(State.All);
+  selectedLabels = signal(new Set<string>()); // { 'a','b','c'}
 
   labelsQuery = injectQuery(() => ({
     queryKey: ['labels'],
@@ -15,11 +16,29 @@ export class IssuesService {
   }));
 
   issuesQuery = injectQuery(() => ({
-    queryKey: ['issues', this.selectedState()],
-    queryFn: () => getIssues(this.selectedState()),
+    queryKey: [
+      'issues',
+      {
+        state: this.selectedState(),
+        selectedLabels: [...this.selectedLabels()],
+      },
+    ],
+    queryFn: () => getIssues(this.selectedState(), [...this.selectedLabels()]),
   }));
 
   showIssuesByState(state: State) {
     this.selectedState.set(state);
+  }
+
+  toggleLabel(label: string) {
+    const labels = this.selectedLabels();
+
+    if (labels.has(label)) {
+      labels.delete(label);
+    } else {
+      labels.add(label);
+    }
+
+    this.selectedLabels.set(new Set(labels));
   }
 }
