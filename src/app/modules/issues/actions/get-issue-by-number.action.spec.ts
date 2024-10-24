1238a15e60476b9303e05a122035b68e730cb82b
C.Manuel Hernández <cmanuelhm87@gmail.com>
2024-10-24 11:54:07 +0200

    Pruebas - GetIssue con error

--- src/app/modules/issues/actions/get-issue-by-number.action.spec.ts
+++ src/app/modules/issues/actions/get-issue-by-number.action.spec.ts
@@ -29,4 +29,27 @@ describe('GetIssueByNumber action', () => {
       },
     });
   });
+
+  it('should not fetch issue successfully', async () => {
+    const requestURL = `${BASE_URL}/issues/${issueNumber}`;
+    const issueResponse = new Response(null, {
+      status: 404,
+      statusText: 'Not Found',
+    });
+
+    spyOn(window, 'fetch').and.resolveTo(issueResponse);
+
+    try {
+      const issue = await getIssueByNumber(issueNumber);
+      expect(true).toBeFalse();
+    } catch (error) {
+      expect(error).toBe(`Can't load issue ${issueNumber}`);
+    }
+
+    // expect(window.fetch).toHaveBeenCalledWith(requestURL, {
+    //   headers: {
+    //     Authorization: `Bearer ${GITHUB_TOKEN}`,
+    //   },
+    // });
+  });
 });
